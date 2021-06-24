9e4c1aef85ffaa1cf294ff6ec440f155c09626e8
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-06-24 19:52:30 -0300

    Port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./app')
 // IMPORTANDO HTTP PARA CRIAÇÃO DO SERVIDOR
 const http = require('http')
 //SETANDO A PORTA
-const port = 3300;
+const port = 3000;
 //CRIAÇÃO DO SERVIDOR
 const server = http.createServer(app);
 //RODAR NA PORTA 3000
