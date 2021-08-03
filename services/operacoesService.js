b2aa27d601750ce1be532ab09b70107d75b7f9da
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-08-02 23:48:04 -0300

    melhorias update

--- services/operacoesService.js
+++ services/operacoesService.js
@@ -5,6 +5,7 @@ const adicionaOperacao = async data => {
     if(data.tag == '' || data.tipoOeracao == '' || data.dataInicio == '' || data.quantidade == '' || data.valorUnitario == ''){
         return error;
     }
+
     var result = await findOperacao(data);
     if(result.length == 0){
          const operacao = new Operacao({
@@ -12,13 +13,13 @@ const adicionaOperacao = async data => {
             tipoOperacao: data.tipoOperacao,
             dataInicio: data.dataInicio,
             dataAtualizacao: new Date(),
-            quantidadeOperacao: data.quantidade,
+            quantidadeOperacao: data.quantidadeAtual,
             valorUnitario: data.valorUnitario,
-            valorTotal: data.valorUnitario*data.quantidade,
+            valorTotal: data.valorUnitario*data.quantidadeAtual,
             quantidadeAtual: data.quantidade,
-            valorTotalAtual: data.valorUnitario*data.quantidade,
+            valorTotalAtual: data.valorUnitario*data.quantidadeAtual,
             verifica: true,
-            valorPrimeiraOperacao: data.valorUnitario*data.quantidade,
+            valorPrimeiraOperacao: data.valorUnitario*data.quantidadeAtual,
             usuario: data.usuario
         })
         return await operacao.save()
@@ -41,22 +42,24 @@ const adicionaOperacao = async data => {
         })
         console.log(atualizaOperacao)
         var resultado = await Operacao.findByIdAndUpdate(result[0]._id, atualizaOperacao, {new: true}).lean();
-        
-        var valorTotal = data.valorUnitario*data.quantidade;
+        console.log(resultado)
+        console.log(result[0]._id)
+        var valorTotal = data.valorUnitario*data.quantidadeAtual;
         const operacao = new Operacao({
             tag: data.tag,
             tipoOperacao: data.tipoOperacao,
             dataInicio: result[0].dataInicio,
             dataAtualizacao: new Date(),
-            quantidadeOperacao: data.quantidade,
+            quantidadeOperacao: data.quantidadeAtual,
             valorUnitario: data.valorUnitario,
             valorTotal: valorTotal,
-            quantidadeAtual: data.tipoOperacao == "Compra" ? result[0].quantidadeAtual+data.quantidade : result[0].quantidadeAtual-data.quantidade,
+            quantidadeAtual: data.tipoOperacao == "Compra" ? result[0].quantidadeAtual+data.quantidadeAtual : result[0].quantidadeAtual-data.quantidadeAtual,
             valorTotalAtual: data.tipoOperacao == "Compra" ? result[0].valorTotalAtual+valorTotal : result[0].valorTotalAtual-valorTotal,
             verifica: true,
             valorPrimeiraOperacao: result[0].valorPrimeiraOperacao,
             usuario: data.usuario
         })
+        console.log(operacao)
         return await operacao.save()
     } 
 }
